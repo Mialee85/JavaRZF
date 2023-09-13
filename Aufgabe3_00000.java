a851d43501e5d8a6e2f34bd365b766676eb01d0e
Mialee <88945741+Mialee85@users.noreply.github.com>
2023-09-13 10:58:16 +0200

    Aufgabe 3 hinzugefügt

---
+++ Aufgabe3_00000.java
@@ -0,0 +1,18 @@
+public class Aufgabe3_00000 {
+	public static void main(String[] args) {
+		//Aufgabe 3
+		//Es soll ein Array mit dem Inhalt eines 52-Blatt-Kartenspiels erstellt werden.
+		//Anschließend soll das Spiel gemischt werden, so dass jede Karte mindestens 1 mal den Platz wechselt.
+		//Abschließend sollen die ersten 4 Karten gezogen und ausgegeben werden.
+		
+		
+		//Die folgenden drei Arrays dürfen nicht verändert werden.
+		
+		String[] farbe = {"Karo", "Herz", "Pik", "Kreuz"};
+		String[] zahl = {"Ass","2","3","4","5","6","7","8","9","10","Bube","Dame","König"};
+		int[] deck = new int[52];
+		
+		//Lösung ab hier einfügen
+		
+	}
+}
