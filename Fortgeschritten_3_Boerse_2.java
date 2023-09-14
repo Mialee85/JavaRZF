8b6c3008aa4444943ec342f88cdfaa27b083fd0b
Linus Wolf <Linus.Wolf@hsrw.org>
2023-09-14 15:52:15 +0200

    Kleine Änderungen

--- Fortgeschritten_3_Boerse_2.java
+++ Fortgeschritten_3_Boerse_2.java
@@ -1,11 +1,12 @@
 public class Fortgeschritten_3_Boerse_2 {
 	public static void main(String[] args) {
 		//Das folgende Array stellt den Kurswert einer Aktie zu verschiedenen Zeitpunkten dar.
-		//Ermitteln Sie den maximalen Gewinn für genau 2 Transaktionen.
+		//Ermitteln Sie den maximalen Gewinn für maximal 2 Transaktionen.
 		//1 Transaktion stellt dabei den Kauf und späteren Verkauf einer Aktie dar.
 		//Es kann sich immer nur 1 Aktie gleichzeitig in ihrem Besitz befinden. Es muss also erst verkauft werden, bevor erneut gekauft werden kann.
 		
 		int[] werte = {160, 42, 130, 174, 102, 197, 101, 186, 87, 42, 169, 26, 86, 92, 62, 95, 122, 110, 133, 47, 58, 126, 179, 98, 123, 196, 149};
 		
+		//Erwartete Lösung: 153 + 170
 	}
 }
