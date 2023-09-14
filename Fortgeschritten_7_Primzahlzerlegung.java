7d4d6b35a601bc23782c68530ebb351fe70f3119
Linus Wolf <Linus.Wolf@hsrw.org>
2023-09-14 23:05:50 +0200

    7 - JS Teillösung

--- Fortgeschritten_7_Primzahlzerlegung.java
+++ Fortgeschritten_7_Primzahlzerlegung.java
@@ -1,5 +1,31 @@
 public class Fortgeschritten_7_Primzahlzerlegung {
 	public static void main(String[] args) {
 		
+		var input = ns.args[0];
+    var primesToTestBoolean = [];
+    var primesToTest = [];
+
+    for (let i = 0; i < (Math.floor(Math.sqrt(input)) + 1); i++) {
+        primesToTestBoolean[i] = true;
+    }
+    primesToTestBoolean[0] = false;
+    primesToTestBoolean[1] = false;
+
+    for (let i = 0; i < primesToTestBoolean.length; i++) {
+        if (!primesToTestBoolean[i]) {
+            continue;
+        }
+        primesToTest.splice(primesToTest.length, 0, i);
+
+        for (let j = i * i; j < primesToTestBoolean.length; j = j + i) {
+            primesToTestBoolean[j] = false;
+        }
+    }
+
+    for (let i = 0; i < primesToTest.length; i++) {
+        while (input % primesToTest[i] == 0 && input > primesToTest[i]){
+            input = input / primesToTest[i];
+        }
+    }
 	}
 }
