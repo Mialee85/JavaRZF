97ba8bfcb55df18dce1212feb45fcebd9f4f3dc8
Linus Wolf <Linus.Wolf@hsrw.org>
2023-09-14 11:11:51 +0200

    changed werte

--- Fortgeschritten_1_Boerse_1.java
+++ Fortgeschritten_1_Boerse_1.java
@@ -5,7 +5,7 @@ public class Fortgeschritten_1_Boerse_1 {
 		//1 Transaktion stellt dabei den Kauf und späteren Verkauf einer Aktie dar.
 		//Es kann sich immer nur 1 Aktie gleichzeitig in ihrem Besitz befinden. Es muss also erst verkauft werden, bevor erneut gekauft werden kann.
 		
-		int[] werte = {20, 133, 179, 140, 46, 102, 14, 197, 14, 75, 84, 14, 194, 98, 195, 124, 15, 21, 55, 161, 48, 173, 34, 126, 28, 154, 35, 21, 17, 10};
+		int[] werte = {20, 133, 179, 140, 46, 102, 197, 14, 75, 84, 14, 194, 98, 195, 124, 15, 21, 55, 161, 48, 173, 34, 126, 28, 154, 35, 21, 17, 10};
 		
 	}
 }
