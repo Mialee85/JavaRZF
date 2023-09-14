b7e985cf2894d8a71e95fd5709ef3e71523e3f0b
Linus Wolf <Linus.Wolf@hsrw.org>
2023-09-14 13:55:33 +0200

    Loesung 2 fertig

--- Fortgeschritten_2_Boerse_Max.java
+++ Fortgeschritten_2_Boerse_Max.java
@@ -7,5 +7,21 @@ public class Fortgeschritten_2_Boerse_Max {
 		
 		int[] werte = {13, 51, 74, 136, 148, 140, 162, 22, 30, 168, 113, 16, 86, 37, 135, 21, 156, 82, 44, 0, 76, 157, 172, 68, 93, 20, 48, 44, 85, 75, 98, 34, 83, 40, 11, 120};
 		
+		//Erwarter Wert 1053.
+
+		int overallDiff = 0;
+		int currentMax = werte[0];
+		int currentMin = werte[0];
+
+		for (int i = 1; i < werte.length; i++) {
+			if (werte[i] > currentMin) {
+				overallDiff = overallDiff + werte[i] - currentMin;
+				currentMax = werte[i];
+			}
+
+			currentMin = werte[i];
+		}
+
+		System.out.println("Maximaler Gewinn: " + overallDiff + ".");
 	}
 }
