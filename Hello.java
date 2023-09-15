bb75cba080641ca1d093b1656d766bf67f374bd8
JosephAss <josephasseburg@outlook.de>
2023-09-15 13:19:24 +0200

    Aufgabe 1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		Scanner sc = new Scanner(System.in);
+		int inputZahl = sc.nextInt();
+		sc.close();
+	
+	}
 }
