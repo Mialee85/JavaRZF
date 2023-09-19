c22ff853a00aaf9e0953ee7c8e5f5298ce28424d
Linus Wolf <Linus.Wolf@hsrw.org>
2023-09-19 13:03:17 +0200

    Loesung 7 fertig

--- Fortgeschritten_7_Primzahlzerlegung.java
+++ Fortgeschritten_7_Primzahlzerlegung.java
@@ -1,31 +1,47 @@
 public class Fortgeschritten_7_Primzahlzerlegung {
-	public static void main(String[] args) {
-		
-		var input = ns.args[0];
-    var primesToTestBoolean = [];
-    var primesToTest = [];
+    public static void main(String[] args) {
 
-    for (let i = 0; i < (Math.floor(Math.sqrt(input)) + 1); i++) {
-        primesToTestBoolean[i] = true;
-    }
-    primesToTestBoolean[0] = false;
-    primesToTestBoolean[1] = false;
+        int input = 3564; // Integer.parseInt(args[0]);
+        int arrayLength = (int) (Math.floor(Math.sqrt(input)) + 2);
+        boolean[] primesToTestBoolean = new boolean[arrayLength];
+        int[] primesToTest = new int[arrayLength];
 
-    for (let i = 0; i < primesToTestBoolean.length; i++) {
-        if (!primesToTestBoolean[i]) {
-            continue;
+        for (int i = 0; i < arrayLength; i++) {
+            primesToTestBoolean[i] = true;
+            primesToTest[i] = 0;
         }
-        primesToTest.splice(primesToTest.length, 0, i);
 
-        for (let j = i * i; j < primesToTestBoolean.length; j = j + i) {
-            primesToTestBoolean[j] = false;
-        }
-    }
+        primesToTestBoolean[0] = false;
+        primesToTestBoolean[1] = false;
 
-    for (let i = 0; i < primesToTest.length; i++) {
-        while (input % primesToTest[i] == 0 && input > primesToTest[i]){
-            input = input / primesToTest[i];
+        for (int i = 0; i < arrayLength; i++) {
+            if (!primesToTestBoolean[i]) {
+                continue;
+            }
+            primesToTest[i] = i;
+
+            for (int j = i * i; j < arrayLength; j = j + i) {
+                primesToTestBoolean[j] = false;
+            }
         }
+
+        int[] solution = new int[arrayLength];
+        int counterSolutionPosition = 0;
+        int inputModified = input;
+
+        for (int i = 0; i < arrayLength; i++) {
+            while (primesToTest[i] != 0 && inputModified % primesToTest[i] == 0 && inputModified >= primesToTest[i]) {
+                inputModified = inputModified / primesToTest[i];
+                solution[counterSolutionPosition] = primesToTest[i];
+                counterSolutionPosition++;
+            }
+        }
+
+        System.out.println("Die Primzahlzerlegung für " + input + " lautet:");
+
+        for (int i = 0; i < counterSolutionPosition - 1; i++) {
+            System.out.print(solution[i] + " * ");
+        }
+        System.out.print(solution[counterSolutionPosition - 1]);
     }
-	}
 }
